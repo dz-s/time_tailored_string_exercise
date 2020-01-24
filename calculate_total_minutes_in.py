c72da3343f67466eca280faff86cfe16827b6b3c
andrei-volkau <andrei@cybergizer.com>
2020-01-24 15:58:24 +0300

    fix: split input sting by one comma OR one space OR one comma followed by any amount of spaces

--- calculate_total_minutes_in.py
+++ calculate_total_minutes_in.py
@@ -9,7 +9,7 @@ class MinutesTest(unittest.TestCase):
         self.assertEqual(calculate_total_minutes_in("all I did today; i 20m, 35m, 2.5h, 3h40m v 40m 35m 1.2h e 30, 60m  "), 662)
 
 def calculate_total_minutes_in(time_tailored_string=string_example):
-    tokens = time_tailored_string.split(";")[1].split(" ")
+    tokens = re.split(',\s*|[,\s]', time_tailored_string.split(";")[1])
     tokens_with_numbers = [x for x in tokens if any(c.isdigit() for c in x)]
     total_minutes = 0
     for token in tokens_with_numbers:
