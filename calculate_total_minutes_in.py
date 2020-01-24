f2aefa243f204ea5b65e6c52aaeb97eee5cae3fc
andrei-volkau <andrei@cybergizer.com>
2020-01-24 16:26:50 +0300

    update module  with the caserelated to an input string that does not contain ';' character and specify an appropriate test case for that

--- calculate_total_minutes_in.py
+++ calculate_total_minutes_in.py
@@ -7,28 +7,33 @@ class MinutesTest(unittest.TestCase):
     def test_calculate_total_minutes_in(self):
         self.assertEqual(calculate_total_minutes_in(), 602)
         self.assertEqual(calculate_total_minutes_in("all I did today; i 20m, 35m, 2.5h, 3h40m v 40m 35m 1.2h e 30, 60m  "), 662)
+        self.assertEqual(calculate_total_minutes_in("all I did today"), 0)
 
 def calculate_total_minutes_in(time_tailored_string=string_example):
-    tokens = re.split(',\s*|[,\s]', time_tailored_string.split(";")[1])
-    tokens_with_numbers = [x for x in tokens if any(c.isdigit() for c in x)]
     total_minutes = 0
-    for token in tokens_with_numbers:
-        if ('h'in token) or ('m'in token):
-            matched_hours = re.search(r'[\d.]+($|(?=[h]))', token)
-            if matched_hours is not None:
-                hours = matched_hours.group(0)
+    if ';' not in time_tailored_string:
+        return total_minutes  
+    else: 
+        tokens = re.split(',\s*|[,\s]', time_tailored_string.split(";")[1])
+        tokens_with_numbers = [x for x in tokens if any(c.isdigit() for c in x)]
+
+        for token in tokens_with_numbers:
+            if ('h'in token) or ('m'in token):
+                matched_hours = re.search(r'[\d.]+($|(?=[h]))', token)
+                if matched_hours is not None:
+                    hours = matched_hours.group(0)
+                else:
+                    hours = 0
+                matched_minutes = re.search(r'[\d]+($|(?=[m]))', token)
+                if matched_minutes is not None:
+                    minutes = matched_minutes.group(0)
+                else:
+                    minutes = 0
             else:
+                minutes = re.search(r'\d+', token).group(0)
                 hours = 0
-            matched_minutes = re.search(r'[\d]+($|(?=[m]))', token)
-            if matched_minutes is not None:
-                minutes = matched_minutes.group(0)
-            else:
-                minutes = 0
-        else:
-            minutes = re.search(r'\d+', token).group(0)
-            hours = 0
-        total_minutes += int(float(hours)*60 + int(minutes))
-    return total_minutes
+            total_minutes += int(float(hours)*60 + int(minutes))
+        return total_minutes
 
 
 if __name__ == "__main__":
